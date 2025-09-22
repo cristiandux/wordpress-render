80bdd8486899921026b4cb258477060fa66befa1
Cristian <cristiandux8@gmail.com>
2025-09-22 20:28:01 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -87,9 +87,7 @@ if ( ! defined( 'WP_DEBUG' ) ) {
 }
 
 define( 'WP_ENVIRONMENT_TYPE', 'local' );
-// Forzar WordPress a usar HTTPS
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
+
 }
 
 define('FORCE_SSL_ADMIN', true);
